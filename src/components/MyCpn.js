8e44590ddcc52e28e8e6c6408b2477ad54563b96
Setsuna2207 <setsuna2207.yae@gmail.com>
2024-10-26 17:20:51 +0700

    #16

---
+++ src/components/MyCpn.js
@@ -0,0 +1,21 @@
+//class cpn
+
+import React from "react";
+
+class MyCpn extends React.Component {
+
+    //JSX
+    render() {
+        return (
+
+            <div> my first component
+
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyCpn;
+
+//function cpn
