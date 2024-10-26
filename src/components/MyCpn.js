87d45a1340db99cca13c67caf44acb11a7a95a29
Setsuna2207 <setsuna2207.yae@gmail.com>
2024-10-26 22:21:21 +0700

    #17

--- src/components/MyCpn.js
+++ src/components/MyCpn.js
@@ -4,13 +4,20 @@ import React from "react";
 
 class MyCpn extends React.Component {
 
+    //state đồng bộ hóa biến JS và giao diện
+    //thay đổi state -> thay đổi hiển thị
+    state = {
+        name: "Tuýn",
+        age: "20",
+        uni: "HAU"
+    };
+
     //JSX
+    //this: ánh xạ vào class đang sử dụng
     render() {
         return (
-
-            <div> my first component
-
-                {Math.random()}
+            <div>
+                My name is {this.state.name} and I'm {this.state.age}
             </div>
         );
     }
